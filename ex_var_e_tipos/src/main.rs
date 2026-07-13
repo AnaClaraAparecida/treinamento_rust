1c3bb7631a6464f947b18588e6b25aebeb0de390
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2026-07-13 19:33:00 -0300

    exercicios

---
+++ ex_var_e_tipos/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // const
+    println!("Hello, world! - {}", x);
+}
--- tipos_de_dados/src/main.rs
+++ tipos_de_dados/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let x:i8 = 99;
+    let x:i16 = 9999;
+    let x:i32 = 9999999999;
+    let x:i64 = 9999999999999999999;
+    printl!("{}", x);
+
+    // "u8" -> aceita apenas numeros positivos "i8" -> aceita numeros negativos tbm
+}
+
+fn main() {
+    let x:f32 = 9.9; // float
+
+    let x:char = 't'; // 1 caractere 
+
+    let x:&str = "ts"; // imutavel msm com o mut
+    let mut x = String::from("ts"); // mutavel com o mut 
+    x += "tsst"; // ex de mudança 
+
+    printl!("{}", x);
 }
