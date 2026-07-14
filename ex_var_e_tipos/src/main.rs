42b54a283614144e70a35ba479034a5f2cd24508
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2026-07-14 17:24:13 -0300

    analise

--- ex_var_e_tipos/src/main.rs
+++ ex_var_e_tipos/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    // const
-    println!("Hello, world! - {}", x);
+    /*
+    Ano de nascimento, subtração pelo ano atual,
+    deve ter a devoluçao da idade da pessoa em questao 
+    */
+
+    let ano_nsc: i16 = 2007;
+    let ano_atual: i16 = 2026;
+
+    let idade: i16 = ano_atual - ano_nsc;  
+    
+    println!("a idade calculada para o ano de {} é {}", ano_nsc, idade);
 }
