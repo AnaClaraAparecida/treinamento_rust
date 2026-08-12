beb3b0f4df15c604220280f3d514c1f0eea758c7
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2026-08-12 16:20:38 -0300

    desenvolvimento de desafio

--- tipos_de_dados/src/main.rs
+++ tipos_de_dados/src/main.rs
@@ -1,23 +1,19 @@
 fn main() {
-    let x:i8 = 99;
-    let x:i16 = 9999;
-    let x:i32 = 999999999;
-    let x:i64 = 99999999999999;
+    let _x: i8 = 99;
+    let _x: i16 = 9999;
+    let _x: i32 = 999999999;
+    let x: i64 = 99999999999999;
 
     println!("{}", x);
     main1()
-
-    // "u8" -> aceita apenas numeros positivos "i8" -> aceita numeros negativos tbm
 }
 
 fn main1() {
-    let x:f32 = 9.9; // float
-
-    let x:char = 't'; // 1 caractere 
-
-    let mut x:&str = "ts"; 
-    let mut x = String::from("ts"); // mutavel com o mut 
-    x += "tsst"; // ex de mudança 
+    let _x: f32 = 9.9;
+    let _x: char = 't';
+    let x: &str = "ts"; // sem mut, pois nunca é modificada
+    let mut x = String::from(x); // pode reaproveitar o valor
+    x += "tsst";
 
     println!("{}", x);
 }
