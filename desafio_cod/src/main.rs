d5c13c3bfdc7f594f585346f25a1d3243b1050ab
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2026-08-12 18:07:02 -0300

    salvando desafio

--- desafio_cod/src/main.rs
+++ desafio_cod/src/main.rs
@@ -1,4 +1,30 @@
+/*  DESAFIO 1
 
+use std::io;
+
+fn main() {
+    let mut entrada = String::new();
+    io::stdin().read_line(&mut entrada).expect("Erro na leitura");
+
+    // Converte a entrada em dois inteiros positivos
+    let valores: Vec<u32> = entrada
+        .split_whitespace()
+        .filter_map(|s| s.trim().parse().ok())
+        .collect();
+
+    let saldo = valores[0];
+    let valor_compra = valores[1];
+
+    if saldo >= valor_compra{
+        println!("Compra aprovada!");
+    }   else{
+        println!("Saldo insuficiente")
+    }
+    // TODO: Verifique se o saldo é suficiente e imprima a mensagem correta ("Compra aprovada" ou "Saldo insuficiente")
+}
+*/
+
+// DESAFIO 2
 use std::io;
 
 fn main() {
@@ -10,7 +36,16 @@ fn main() {
 
         // TODO: Se houver exatamente dois elementos, imprima a mensagem personalizada.
         // Caso contrário, imprima "Invalid input."
-    }
 
+    if parts.len() == 2{
+        let nome = parts[0];
+        let tipo = parts[1];
+        println!("Welcome, {nome}! Your account type is {tipo}.")
+    } else {
+        println!("Invalid input.")
+    }
     
+
+}
+
 }
